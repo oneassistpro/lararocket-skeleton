c3e6aeb9aa765b217512916d5cb7bcf16f046fbf
Ashok <ashokbaruaakas@gmail.com>
2025-01-18 12:24:33 +0600

    fix: ensure safe code with larastan

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -44,12 +44,12 @@ final class NewPasswordController extends Controller
 
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
+        // database. Otherwise, we will parse the error and return the response.
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function ($user) use ($request) {
                 $user->forceFill([
-                    'password' => Hash::make($request->password),
+                    'password' => Hash::make(is_string($request->password) ? $request->password : ''),
                     'remember_token' => Str::random(60),
                 ])->save();
 
@@ -65,7 +65,9 @@ final class NewPasswordController extends Controller
         }
 
         throw ValidationException::withMessages([
-            'email' => [trans($status)],
+            'email' => [
+                is_string($status) ? trans($status) : $status,
+            ],
         ]);
     }
 }
