03f27945a7a7c82181faad81494eb561c09f4246
Ashok <ashokbaruaakas@gmail.com>
2025-01-18 13:39:35 +0600

    refactor(postcss): moved postcss config inside vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 import laravel from 'laravel-vite-plugin'
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), autoprefixer()],
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.ts',
